cccd84544392c7856a6b7527a15162201fd80999
Maria Koszucka <koszucka.maria@gmail.com>
2022-08-16 17:02:54 +0200

    Prepare base styling based on AdminLTE module

---
+++ src/app/control-sidebar/control-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-sidebar',
+  templateUrl: './control-sidebar.component.html',
+  styleUrls: ['./control-sidebar.component.css']
+})
+export class ControlSidebarComponent implements OnInit {
+  currentProgram = "Program nr 1";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent implements OnInit {
+  menuItem1 = "menuItem1";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
